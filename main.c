910fd9e0eaae1e470ae73d737a01ab2138fafc1e
David Crawford <crawford@mail.gvsu.edu>
2016-08-25 02:04:12 -0400

    Added a prompt for the human player to enter a move, as well as logic for validating user input and changing sides to the AI when the player's move is finished.

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 /* What the board theoretically looks like to the human mind. This is useful for me to keep track of what I'm laying out.
  ; ; ; ; ; ; ;
@@ -12,7 +15,7 @@
 
 enum { empty, nought, cross, border }; //It's good to use enumerators here because these variables all increment from 0
 
-const int convertTo48[25] = { //Only listing the array values that reside within the borders
+const int convertTo49[25] = { //Only listing the array values that reside within the borders
     8,9,10,11,12,
     15,16,17,18,19,
     22,23,24,25,26,
@@ -28,7 +31,7 @@ void initializeBoard(int *board) {
     }
     
     for (i = 0; i < 25; ++i) { //Then it filters out what is in-between
-        board[convertTo48[i]] = empty;
+        board[convertTo49[i]] = empty;
     }
 }
 
@@ -46,10 +49,75 @@ void printBoard(const int *board) {
     printf("\n");
 }
 
-int main(void) {
+int hasEmpty(const int *board) {
+    int i = 0;
+    int empty = 0;
+    
+    for(i = 0; i < 25; ++i) {
+        if(board[convertTo49[i]] == empty)
+            return 1;
+    }
+    return 0;
+}
+
+void makeMove(int *board, const int square, const int side) {
+    board[square] = side;
+}
+
+
+int getHumanMove(const int *board) {
+    char userInput[4]; //4 chars because we'll only receive 2 inputs from user: a number and the enter key
+    
+    int moveOk = 0;
+    int move = -1;
+    
+    while (moveOk == 0) {
+        printf("Enter a number between 1 and 25: ");
+        fgets(userInput, 3, stdin);
+        fflush(stdin); //Gets rid of extra strings after fgets retreives the first 3
+        moveOk = 1;
+    }
+    
+    printf("Making move...%d\n", (move + 1));
+    return convertTo49[move];
+}
+
+void runGame() {
+    int gameOver = 0;
+    int side = nought;
+    int lastMoveMade = 0;
     int board[49];
+    
     initializeBoard(&board[0]);
     printBoard(&board[0]);
     
+    while(!gameOver) {
+        if(side==nought) {
+            getHumanMove(&board[0]);
+        }
+        else {
+            printBoard(&board[0]);
+        }
+        
+        gameOver = 1;
+    }
+}
+
+int main(void) {
+    
+    srand(time(NULL)); //Random seed generation
+    runGame();
+    
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
